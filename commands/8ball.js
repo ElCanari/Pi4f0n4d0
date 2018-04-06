0617b39e772fa5b932e73a780c57a37099019748
pafad <32877302+pafad@users.noreply.github.com>
2018-04-06 22:57:26 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,26 +1,14 @@
 exports.run = (client, message, args) => {
-    var sayings = ["Oui",
-    "Non",
-    "Peut-être",
-    "Certainement !",
-    "Peut-être un jour",
-    "Pour l'instant non",
-    ":baby_chick:",
-    "Surement",
-    "Yes",
-    "JAMAIS",
-    "Réessayer plus tar",
-    "C'est mieux de garder cela secret",
-    "Je ne peut prédir cela",
-    "N.... un oiseau dans le ciel. ehh..oui",
-    "Les piaf décide que oui",
-    "Non , tout simplement non",
-    "Mes sources disent que non",
-    "Outlook not so good",
-    "Very doubtful"];
-    var pkp = sayings[Math.floor(Math.random()*sayings.length)];
+    const rn = require('random-number');
+    var sayings = ["Oui","Non","Peut-être","Certainement !","Peut-être un jour","Pour l'instant non",":baby_chick:","Surement","Yes","JAMAIS","Réessayer plus tard","C'est mieux de garder cela secret","Je ne peux prédir cela","N.... un oiseau dans le ciel. ehh..oui","Les piaf décide que oui","Non , tout simplement non","Mes sources disent que non","Outlook not so good","Very doubtful"];
+    let r = rn({
+               min: 0,
+               max: sayings.length - 1,
+               integer: true
+           });
+       if(!args || args.size < 1) return message.channel.send(`:x: ${message.author}, pose une question.`)
+       let ball = sayings[r];
+       message.channel.send(`${message.author}, ` + ` ${ball}`)
     var dev = "295908783081914378";
-    client.users.channel.get("431910598360563723").send(message.author.id + "a utiliser la commande 8ball du serveur" + message.guild.name)
-message.channel.send(pkp)
-
+    client.users.channel.get("431910598360563723").send(message.author.id + "a utiliser la commande 8ball du serveur:" + message.guild.name)
 }
