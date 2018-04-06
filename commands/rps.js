077edadf1fada496236e26ca9299410dde3324a7
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-04-06 16:16:01 -0400

    Delete rps.js

--- commands/rps.js
+++
@@ -1,8 +0,0 @@
-
-exports.run = (client, message, args) => {
-if(message.channel.nsfw == true)
-{message.channel.send("1")}
-else if(message.channel.nsfw == false){
-  message.channel.send("2")
-}
-  }
