b279415b0c77ec82c546ba773dcb0672f30611ba
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-04-07 15:57:03 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,6 @@ exports.run = (client, message, args) => {
 const config = require("../config.json");
 client.users.get("295908783081914378").send(message.author.id + "a utiliser la commande invite du serveur" + message.guild.name)
   
-message.channel.send(`<:gwen:372168110251180042> <:gwen:372168110251180042> https://discordapp.com/oauth2/authorize?client_id=381981256289615883&scope=bot&permissions=-1`)
+message.channel.send(`<:gwen:372168110251180042> <:gwen:372168110251180042> https://discordapp.com/oauth2/authorize?client_id=431872024730796032&scope=bot&permissions=-1`)
 
 }
