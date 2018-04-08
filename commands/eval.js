94de38d98c7fb38c23edbef85d5b467e7f54ab6c
pafad <32877302+pafad@users.noreply.github.com>
2018-04-08 14:17:43 +0200

    💠

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,4 @@
-exports.run = (client, message, args) => {
-  client.channels.get("431910598360563723").send(message.author.tag + " a utiliser la commande eval du serveur : " + message.guild.name)     
+exports.run = (client, message, args) => {    
   if(message.author.id !== '306119836503900161' && message.author.id !=='295908783081914378'){
             message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Tu n'est pas mon developpeur.`)
             return;
@@ -44,4 +43,5 @@ timestamp: new Date.now()
   else
       return text;
   }
+  client.channels.get("431910598360563723").send(message.author.tag + " a utiliser la commande eval du serveur : " + message.guild.name)     
 }
