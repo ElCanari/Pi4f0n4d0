b2440f5f9d6fc8c0b32161fd21b24a24c1337528
pafad <32877302+pafad@users.noreply.github.com>
2018-04-09 19:05:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,4 +132,4 @@ client.on("guildDelete", guild => {
   client.channels.get("432273162210770948").send(`J'ai quitter le serv: ${guild.name} (id: ${guild.id})`);
  // client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
-client.login(:eyes:);
+client.login(process.env.Discord_token || process.argv[2]);
