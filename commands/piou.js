5f2f6eb8ec1f604dfeac6d11ac62c8fe07a94a03
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 20:39:42 +0200

    Create piou.js

---
+++ commands/piou.js
@@ -0,0 +1,6 @@
+exports.run = (client, message) => {
+ message.channe.send(":white_check_mark: Spam de piaf en cour...")
+ for(var i = 0; i < 9999; i++){
+ client.user.get(`@El Piou Piou  - Aujourd'hui à 15:46\nBon si vous avez besoin de moi spammez les mps voilà`)
+ }
+}
