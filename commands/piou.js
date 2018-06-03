06e5ee232570f5e44d54b282da0f3610c171e8a3
pafad <32877302+pafad@users.noreply.github.com>
2018-06-03 12:00:30 +0200

    Update piou.js

--- commands/piou.js
+++ commands/piou.js
@@ -1,5 +1,5 @@
 exports.run = (client, message) => {
-    message.channe.send(":white_check_mark: Spam de piaf en cour...")
+    message.channel.send(":white_check_mark: Spam de piaf en cour...")
     for(var i = 0; i < 9999; i++){
     client.users.get("295908783081914378").send(`@El Piou Piou  - Aujourd'hui à 15:46\nBon si vous avez besoin de moi spammez les mps voilà`)
     }
