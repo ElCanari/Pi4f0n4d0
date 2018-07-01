6b9d7e9ba1c842b6d58561252e969f5921b194f4
pafad <32877302+pafad@users.noreply.github.com>
2018-07-01 12:59:33 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,15 +1,29 @@
 exports.run = (client, message, args) => {
-let test123 =  message.mentions.members.first();
-client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande invite du serveur" + message.guild.name)
-let reason = args.slice(1).join(" ");
-if(message.member.hasPermission("BAN_MEMBERS"))
-{
-message.channel.send("Tu a réussi a kick " + test123)
-test123.kick(reason)
-}
-else
-{
-message.channel.send("Assurez vous d'avoir les permissions suffisant ou que j'ai les permissions sufisante pour bannir")
+        let raison = message.content.substr(28);
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!message.member.hasPermissions("KICK_MEMBERS")){
+            message.channel.send(`:x: ${message.author}, Tu n'as pas la permission de kick les membres.`)
+            return;
+        }else{
+            if(!kickMember){
+                message.channel.send(`:x: ${message.author}, veuillez mentionner un utilisateur.`)
+                return;
 
-}
+          }else{
+              if(!kickMember.kickable){
+                  message.channel.send(`:x: ${message.author}, je peux pas kick cet utilisateur veuillez vérifier mes rôles et permissions.`)
+                  return;
+            }else{
+                if(kickMember.hasPermission("ADMINISTRATOR")){
+                    message.channel.send(`:x: ${message.author}, cet utilisateur est un admin, je peux pas faire ça.`)
+                    return; 
+                }else{
+        message.delete(message.author);
+        client.users.get(kickMember.id).send(`tu as été kick pour la raison: ${raison}`)
+        message.guild.member(kickMember).kick({reason: `${raison}`});
+        message.channel.sendMessage(`**${kickMember}** a été kick! raison:${raison}`);
+                }
+            }
+        }
+    }
 }
