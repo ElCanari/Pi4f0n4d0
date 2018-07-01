b406b9bbc7e404d95c05acac45da61f65bb3fd42
pafad <32877302+pafad@users.noreply.github.com>
2018-07-01 13:02:05 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,15 +1,29 @@
-exports.run = (client, message, args) => {
-let test123 =  message.mentions.members.first();
-client.channels.get("431910598360563723").send(message.author.tag + " a utiliser la commande ban du serveur: " + message.guild.name)
-let reason = args.slice(1).join(" ");
-if(message.member.hasPermission("BAN_MEMBERS"))
-{
-message.channel.send("Tu a réussi a bannir " + test123)
-test123.ban(reason)
-}
-else
-{
-message.channel.send("Assurez vous d'avoir les permissions suffisant ou que j'ai les permissions sufisante pour bannir")
+exports.run = (client, message, args)  => {
+        let raison = message.content.substr(27);
+        let banMember = message.guild.member(message.mentions.users.first());
+        if(!message.member.hasPermissions("BAN_MEMBERS")){
+            message.channel.send(`:x: ${message.author} Tu n'as pas la permission de ban les membres.`)
+            return;
+        }else{
+            if(!banMember){
+                message.channel.send(`:x: ${message.author} veuillez mentionner un utilisateur.`)
+                return;
 
-}
-}
+          }else{
+              if(!banMember.bannable){
+                  message.channel.send(`:x: ${message.author} je peux pas kick cet utilisateur veuillez vérifier mes rôles et permissions.`)
+                  return;
+            }else{
+                if(banMember.hasPermission("ADMINISTRATOR")){
+                    message.channel.send(`:x: ${message.author} cet utilisateur est un admin, je peux pas faire ça.`)
+                    return; 
+                }else{
+        message.delete(message.author);
+        client.users.get(kickMember.id).send(`tu as été ban pour la raison: ${raison}`)
+        message.guild.member(banMember).ban({reason: `${raison}`});
+        message.channel.sendMessage(`**${banMember}** a été ban! raison:${raison}`);
+                }
+            }
+          }
+       }
+    }
