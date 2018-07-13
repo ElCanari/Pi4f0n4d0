a2cbcd03a16cbb73fa673e8f42b71c801ea13abf
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:24:15 +0200

    Update changegame.js

--- commands/changegame.js
+++ commands/changegame.js
@@ -1,9 +1,13 @@
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
 const config = require("../config.json");
-client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande changegame du serveur" + message.guild.name)
+
 const ped = args.join(" ");
     if(message.author.id !== "295908783081914378" && message.author.id !== "317221808405348364" ) return;
   
 message.channel.send(":white_check_mark: ")
 client.user.setActivity(ped)
+}
+
+module.exports.help = {
+    name:"changegame"
 }
