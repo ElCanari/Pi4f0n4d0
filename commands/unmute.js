7f9ea7b5760f54f4859caa3a0e6ec18d11e1442d
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:58:47 +0200

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,11 +1,10 @@
-   exports.run = (client, message, args) => {
-  let member = message.mentions.members.first();
+module.exports.run = async (client, message, args) => {
+  let memberMute = message.mentions.members.first();
    let role = message.guild.roles.find("name", "Mute");
-   client.user.get("295908783081914378").send(message.author.id + "a utiliser la commande unmute du serveur" + message.guild.name)
    if (message.member.hasPermission("MANAGE_MESSAGES"))
    {
-	  member.removeRole(role).catch(console.error); 
-	  message.channel.send(member + " a été unmute");
+	  memberMute.removeRole(role).catch(console.error); 
+	  message.channel.send("**" + memberMute.tag + "** a été unmute");
    }   
    else 
    {
@@ -14,3 +13,6 @@
    }
 
    }
+module.exports.help = {
+	name:"unmute"
+}
