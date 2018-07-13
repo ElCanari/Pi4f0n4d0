fd8e4419ce0023d359d1ad16098498497aefc4de
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:36:15 +0200

    Update loverate.js

--- commands/loverate.js
+++ commands/loverate.js
@@ -1,9 +1,11 @@
-
-exports.run = (client, message, args) => {
-    client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande loverate du serveur" + message.guild.name)
+module.exports.run = async (client, message, args) => {
     //  message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
     var pkp = [Math.floor(Math.random()*100)];
     let t1 = args[0]
     let t2 = args[1]
 message.channel.send(pkp + "% d'amour entre " + t1 + "et " + t2)
+}
+
+module.exports.help = {
+    name:"loverate"
 }
