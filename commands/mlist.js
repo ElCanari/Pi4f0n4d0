82d927f187ab2d35eb6b9913d0fe578c7cf959a3
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:41:39 +0200

    Update mlist.js

--- commands/mlist.js
+++ commands/mlist.js
@@ -1,6 +1,8 @@
-
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
 const mlist = message.guild.members.map(g => g.user.username)
-client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande mlist du serveur" + message.guild.name)
     message.channel.send(mlist).catch(console.error);
+}
+
+module.exports.help = {
+    name:"mlist"
 }
