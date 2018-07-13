30797e211676a3bc68c05fd38e3253a4574bd5dd
pafad <32877302+pafad@users.noreply.github.com>
2018-07-14 00:22:07 +0200

    Update setnick.js

--- commands/setnick.js
+++ commands/setnick.js
@@ -1,15 +1,15 @@
 module.exports.run = async (client, message, args) => {
      let user_setnick = message.mentions.users.first();
     if(!message.member.hasPermission("MANAGE_NICKNAMES")){
-        message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu n'as pas la permission de gérer les pseudo"`)
+        message.channel.send(`:baby_chick: ${message.author} tu n'as pas la permission de gérer les pseudo"`)
         return;
     }else{
     if(!user_setnick){
-        message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Veuillez mentionner un utilisateur`)
+        message.channel.send(`:bird: Veuillez mentionner un utilisateur`)
         return;
     }else{
-       message.guild.members.get(user_setnick.id).setNickname(message.content.substr(31))
-        message.channel.send(`<:7orNad0_check_mark:400045879958175745> pseudo changé ! nouveau pseudo: ${message.content.substr(32)}`)
+       message.guild.members.get(user_setnick.id).setNickname(message.content.substr(32))
+        message.channel.send(`:baby_chick: pseudo changé ! nouveau pseudo: ${message.content.substr(32)}`)
         }
     }
 }
