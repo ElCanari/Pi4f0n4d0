4d117f1c5813b436402d84155800139c8a5f042f
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:27:43 +0200

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -1,8 +1,12 @@
-exports.run = (client, message, args) => {
+module.exports.run = (client, message, args) => {
 const config = require("../config.json");
-client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande close du serveur" + message.guild.name)
+
     if(message.author.id !== "295908783081914378" || message.author.id !== "295908783081914378") return message.channel.send("tu n'es pas l'un de mes developpeurs.")
   
 message.channel.send("bot down")
 process.exit(0);
+}
+
+module.exports.help = {
+    name:"close"
 }
