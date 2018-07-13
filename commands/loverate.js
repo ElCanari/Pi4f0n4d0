07cc99ca69858035241102fc0d97be32772e4c9f
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 23:37:34 +0200

    Update loverate.js

--- commands/loverate.js
+++ commands/loverate.js
@@ -1,5 +1,4 @@
-module.exports.run = async (client, message, args) => {
-    //  message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
+module.exports.run = async (client, message, args) => {       
     var pkp = [Math.floor(Math.random()*100)];
     let t1 = args[0]
     let t2 = args[1]
