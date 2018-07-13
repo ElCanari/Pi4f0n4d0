9653bc5a250d6a3a22f71d5fd4c553fe5491b96f
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:28:43 +0200

    Update dragon.js

--- commands/dragon.js
+++ commands/dragon.js
@@ -1,5 +1,8 @@
-exports.run = (client, message, args) => {
+module.exports.run = (client, message, args) => {
     message.channel.send("Dragon ? :dragon: <<<<<< all").catch(console.error);
     message.react(":PandaDevil:385523844208656394");
-    client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande dragon du serveur" + message.guild.name)
+}
+
+module.exports.help = {
+    name:"dragon"
 }
