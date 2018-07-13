320957b3ae7c42f200fc5eb3323a9e831f2f080a
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:30:13 +0200

    Update elist.js

--- commands/elist.js
+++ commands/elist.js
@@ -1,5 +1,8 @@
-  exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
   const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
   message.channel.send(emojiList);
-  client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande elist du serveur" + message.guild.name)
+}
+
+module.exports.help = {
+  name:"elist"
 }
