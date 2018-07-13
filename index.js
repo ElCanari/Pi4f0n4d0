0cd4af99a8f493cf16132dedebcb8f34b6f2007e
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 23:23:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const prefix = config.prefix;
 const prefix2 = ">";
 let type = 1;
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 //best handler ever
 fs.readdir("./commands/", (err, files) => {
   
