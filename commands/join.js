133da64e0eb05de494b8afa027551a6837c5135a
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 22:33:49 +0200

    Delete join.js

--- commands/join.js
+++
@@ -1,7 +0,0 @@
-exports.run = () => {
-return new Promise((resolve, reject) => {
-			const voiceChannel = message.member.voiceChannel;
-			if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply("Je n'ai pas la permission de me connecter dans ton channel vocal...");
-			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-		});
-}
