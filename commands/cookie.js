43251fd85454ce5d67d54be05a0e18f94f546817
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 23:29:46 +0200

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -1,11 +1,10 @@
-
-
-exports.run = (client, message, args) => {
+module.exports.run = (client, message, args) => {
   let member = message.mentions.members.first();
-  client.channels.get("431910598360563723").send(message.author.tag + "a utiliser la commande cookie du serveur" + message.guild.name)
  message.channel.send( member + " ** A reçu un cookie de ** " + message.author, {
     file: "http://www.pngall.com/wp-content/uploads/2016/07/Cookie-Download-PNG.png" // Or replace with FileOptions object
 }).catch(console.error);
+}
 
-	
-	}
+module.exports.help = {
+	name:"cookie"
+}
