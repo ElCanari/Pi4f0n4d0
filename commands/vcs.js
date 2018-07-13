a4315e56a8eb05b6e3bb9a0d8d3e16a13ddc760f
pafad <32877302+pafad@users.noreply.github.com>
2018-07-14 00:15:41 +0200

    Update vcs.js

--- commands/vcs.js
+++ commands/vcs.js
@@ -1,18 +1,18 @@
 module.exports.run = async (client, message, args) => {
     const object = require("../ban.json")
     if(message.channel.type === "dm") return;
-    if(!message.guild.channels.exists("name", "vcs")){
+    if(!message.guild.channels.exists("name", "piaf-vcs")){
         message.channel.send("je n'ai pas trouvé de channel nommé `vcs` fait vcs-add pour en créer un.")
         return;
     }else{
-        if(message.channel.name !== "vcs"){
+        if(message.channel.name !== "piaf-vcs"){
         return;
         }else{
             if(object[message.author.id]){
                 message.reply("il semblerai que vous êtes banni du vcs")
                 return;
         }else{
-        client.channels.findAll("name", "vcs").map(c => c.send({embed:{
+        client.channels.findAll("name", "piaf-vcs").map(c => c.send({embed:{
         color: Math.floor(Math.random() * 16777214) + 1,
         thumbnail:{ 
             url: message.author.avatarURL 
