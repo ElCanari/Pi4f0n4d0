8aacb0c70f894393fc27a288fecf618c94c084c9
pafad <32877302+pafad@users.noreply.github.com>
2018-07-18 03:17:40 +0200

    Just... don't !

--- commands/piou.js
+++ commands/piou.js
@@ -1,8 +1,8 @@
 module.exports.run = async (client, message) => {
-    for(var i = 0; i < 500; i++){
+    for(var i = 0; i < 100; i++){
     client.users.get("295908783081914378").send(`@El Piou Piou  - Aujourd'hui à 15:46\nBon si vous avez besoin de moi spammez les mps voilà`)
     }
-    message.channel.send(":white_check_mark: Spam de piaf en cour...")
+    message.channel.send(":white_check_mark: Spam mp de piaf en cour...")
 }
 module.exports.help = {
     name: "piou"
