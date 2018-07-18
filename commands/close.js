222d8dcc77c3da3a5d22798f6561a822f0438586
pafad <32877302+pafad@users.noreply.github.com>
2018-07-18 03:16:15 +0200

    lol

--- commands/close.js
+++ commands/close.js
@@ -4,7 +4,9 @@ const config = require("../config.json");
     if(message.author.id !== "295908783081914378" || message.author.id !== "295908783081914378") return message.channel.send("tu n'es pas l'un de mes developpeurs.")
   
 message.channel.send("bot down")
-process.exit(0);
+process.exit(1);
+client.destroy();
+client.login(process.env.Discord_token);
 }
 
 module.exports.help = {
