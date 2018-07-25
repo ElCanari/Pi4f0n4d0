6092df6cedd8131953f110f8533683d9ea018617
pafad <32877302+pafad@users.noreply.github.com>
2018-07-25 22:29:36 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ module.exports.run = async (client,message, args) => {
     color: 0x9101ff,
     title: "Tu veux m'inviter voici le lien:",
     url: '',
-    description: "https://discordapp.com/oauth2/authorize?client_id=453648303528345611&scope=bot&permissions=-1",
+    description: "https://discordapp.com/oauth2/authorize?client_id=433003942000197632&scope=bot&permissions=-1",
     footer: {
       icon_url: client.user.avatarURL,
       text: `${client.user.username} by shiro`
