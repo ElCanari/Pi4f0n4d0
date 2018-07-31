f4bbe9b050d9f4eda76f8697014def972781554b
pafad <32877302+pafad@users.noreply.github.com>
2018-07-31 14:15:07 +0200

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
       message.channel.send(":x: écris quelque chose d'important à annoncer")
       return;
       }
-        message.guild.members.filter(m => m.roles.find("name", args.join(" "))).map(u => u.send(message.content.substr(message.guild.roles.find("name", args.join(" ")).name.length + 1)))
+        message.guild.members.filter(m => m.roles.find("name", args)).map(u => u.send(message.content.substr(args.length + 1)))
         message.channel.send(":baby_chick: annonce envoyée !")
       }
       }else{
