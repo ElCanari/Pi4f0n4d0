11cd5e2868e7b0f453a956e77d76c9cc6e4be785
pafad <32877302+pafad@users.noreply.github.com>
2018-07-31 19:26:13 +0200

    Update bltr.js

--- commands/bltr.js
+++ commands/bltr.js
@@ -9,25 +9,27 @@ module.exports.run = async (client, message) => {
                 if(err || res.statusCode!== 200)return
         
                 console.log('chargé avec succés')
-                let Blchannel = JSON.parse(body);  
-if(Blchannel[message.channel.id].boonlean === true){
-   if(!message.member.hasPermission("ADMINISTRATOR")){
-    message.channel.send(":x: Tu n'as pas les permissions nécéssaires.")
-    return;
-    }else{
-   Blchannel[message.channel.id].boonlean = false;
-   request({ url: cblUrl, method: 'PUT', json: Blchannel})
-   message.channel.send("les commandes de trésor ne sont plus blacklist ici")
-    return;
-}
-}else{
-    if(!message.member.hasPermission("ADMINISTRATOR")){
-        message.channel.send(":x: Tu n'as pas les permissions nécéssaires.")
-        }else{
-        if(!Blchannel[message.channel.id]) Blchannel[message.channel.id] = {};
-        if(!Blchannel[message.channel.id].boonlean) Blchannel[message.channel.id].boonlean = true;
-        message.channel.send("commande trésor blacklist dans ce channel")
-        request({ url: cblUrl, method: 'PUT', json: Blchannel})
+                let Blchannel = JSON.parse(body);
+                if(!Blchannel[message.channel.id]) Blchannel[message.channel.id] = {};
+                if(!Blchannel[message.channel.id].boonlean) Blchannel[message.channel.id].boonlean = false;
+                if(Blchannel[message.channel.id].boonlean === true){
+                if(!message.member.hasPermission("ADMINISTRATOR")){
+                message.channel.send(":x: Tu n'as pas les permissions nécéssaires.")
+                return;
+                }else{
+                Blchannel[message.channel.id].boonlean = false;
+                request({ url: cblUrl, method: 'PUT', json: Blchannel})
+                 message.channel.send("les commandes de trésor ne sont plus blacklist ici")
+                 return;
+                 }
+                    }else{
+                if(!message.member.hasPermission("ADMINISTRATOR")){
+                message.channel.send(":x: Tu n'as pas les permissions nécéssaires.")
+                }else{
+                if(!Blchannel[message.channel.id]) Blchannel[message.channel.id] = {};
+                if(!Blchannel[message.channel.id].boonlean) Blchannel[message.channel.id].boonlean = true;
+                message.channel.send("commande trésor blacklist dans ce channel")
+                request({ url: cblUrl, method: 'PUT', json: Blchannel})
     }
 }
 })
