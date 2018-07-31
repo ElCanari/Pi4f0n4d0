c05cc0579a8c405522c4eb40977beb6b126af9ce
pafad <32877302+pafad@users.noreply.github.com>
2018-07-31 14:29:35 +0200

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -1,19 +1,18 @@
 module.exports.run = async (client, message, args) => {
     if(message.guild.id === process.env.rgk){
-      if(message.member.hasPermission("MANAGE_MESSAGES")){
-      if(!args[0]){
-      message.channel.send(":x: écris quelque chose d'important à annoncer")
-      return;
-      }
-        message.guild.members.filter(m => m.roles.find("id", args[0])).map(u => u.send(message.content.substr(args[0].length)))
-        message.channel.send(":baby_chick: annonce envoyée !")
-      }
+      if(!message.member.hasPermission("MANAGE_MESSAGES")){
+        message.channel.send(":x: Tu n'as pas la permission.")
+        return;
       }else{
-      message.channel.send(":x: Tu n'as pas la permission.")
-      }
+      if(!args[0]) return message.channel.send(":x: écris quelque chose d'important à annoncer");
+      message.guild.members.filter(m => m.roles.find("id", args[0])).map(u => u.send(message.content.substr(args[0].length)))
+      message.channel.send(":baby_chick: annonce envoyée !")
+        }
+    }else{
+    return;
     }
+}
   
   module.exports.help = {
         name:"annonce"
   }
-  
