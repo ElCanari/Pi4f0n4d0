31c3e6d4fd82acd976ec335a0e0ab41e4e91c19f
pafad <32877302+pafad@users.noreply.github.com>
2018-07-31 10:15:02 +0200

    Update t.js

--- commands/t.js
+++ commands/t.js
@@ -51,12 +51,9 @@ module.exports.run = async (client, message, args) => {
                     userData[Sender.id + message.guild.id].comboTr++;
                     request({ url: url, method: 'PUT', json: userData})
                     message.reply("Tu eu le trésor de guilde ton combo actuel monte à " + userData[Sender.id + message.guild.id].comboTr + " !");
-                    Tr.taker = Sender.tag;
                     Tr.time = Date.now() + Math.floor(Math.random()+6000000);
                     request({ url: trUrl, method: 'PUT', json: Tr})
-                }
-            }
-        }
+              }
         })
     })
 
