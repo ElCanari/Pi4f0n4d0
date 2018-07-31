dc0ecd0eaa87d688a4bf28d8be417a3b0a118c7d
pafad <32877302+pafad@users.noreply.github.com>
2018-07-31 12:04:59 +0200

    Create annonce.js

---
+++ commands/annonce.js
@@ -0,0 +1,18 @@
+module.exports.run = async (client, message, args) => {
+  if(message.guild.id === process.env.rgk){
+    if(message.member.hasPermission("MANAGE_MESSAGES")){
+    if(!args){
+    message.channel.send(":x: écris quelque chose d'important à annoncer")
+    }
+    message.guild.members.map(c => c.send(message.content.substr(10)))
+    }else{
+    message.channel.send(":x: Tu n'as pas la permission.")
+    }
+    }else{
+    return;
+    }
+}
+
+module.exports.help = {
+      name:"annonce"
+}
