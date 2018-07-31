4b101bb4d9a9543a4f50c646ba3b32189281da3f
pafad <32877302+pafad@users.noreply.github.com>
2018-07-31 12:43:11 +0200

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -1,19 +1,24 @@
 module.exports.run = async (client, message, args) => {
-  if(message.guild.id === process.env.rgk){
-    if(message.member.hasPermission("MANAGE_MESSAGES")){
-    if(!args){
-    message.channel.send(":x: écris quelque chose d'important à annoncer")
+    if(message.guild.id === process.env.rgk){
+      if(message.member.hasPermission("MANAGE_MESSAGES")){
+      if(!args){
+      message.channel.send(":x: écris quelque chose d'important à annoncer")
+      return;
+      }
+        if(message.guild.roles.findKey(args[0])){
+        message.guild.members.filter(message.guild.roles.findKey(args[0])).map(c => c.send(`${message.content.substr(args[0].length + 1)} de ${message.author}`))
+        message.channel.send(":baby_chick: annonce envoyée !")
+        }else{
+        message.guild.members.map(c => c.send(`${message.content.substr(10)} de ${message.author}`))
+        message.channel.send(":baby_chick: annonce envoyée !")
+        }  
+      }
+      }else{
+      message.channel.send(":x: Tu n'as pas la permission.")
+      }
     }
-    message.guild.members.map(c => c.send(`${message.content.substr(10)} de ${message.author}`))
-    message.channel.send(":baby_chick: annonce envoyée !")
-    }else{
-    message.channel.send(":x: Tu n'as pas la permission.")
-    }
-    }else{
-    return;
-    }
-}
-
-module.exports.help = {
-      name:"annonce"
-}
+  
+  module.exports.help = {
+        name:"annonce"
+  }
+  
