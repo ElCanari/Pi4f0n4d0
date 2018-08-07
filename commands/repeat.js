98c9d42d0b6ad711c375456d3fe3079c6d63d87a
pafad <32877302+pafad@users.noreply.github.com>
2018-08-07 19:17:55 +0200

    Update repeat.js

--- commands/repeat.js
+++ commands/repeat.js
@@ -2,19 +2,10 @@ module.exports.run = async (client, message, args) => {
     if(!args[0]){
         message.channel.send("choisi un nombre de répétitions");
         return;
+    
     }
-    if(args[0] = 0 || args[0] > 99){
-        message.channel.send("choisi un nombre entre 1 et 99")   
-    }
-    if(args[0] > 9){
-    for(var i = 0; i < args[0]; i++){
-        message.channel.send(message.content.substr(12))
-    }
-    }
-    if(args[0] < 10){
-    for(var i = 0; i < args[0]; i++){
-        message.channel.send(message.content.substr(11))
-    }
+    for(var i = 0; i < 25; i++){
+        message.channel.send(message.content.substr(9));
     }
     message.channel.send(":white_check_mark: répétitions en cour...")
 }
