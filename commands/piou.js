aee261bc0c0984eb821b2a62726e98e2f371884f
pafad <32877302+pafad@users.noreply.github.com>
2018-08-07 18:54:36 +0200

    Update and rename piou.js to repeat.js

--- commands/piou.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (client, message) => {
-    for(var i = 0; i < 100; i++){
-    client.users.get("295908783081914378").send(`@El Piou Piou  - Aujourd'hui à 15:46\nBon si vous avez besoin de moi spammez les mps voilà`)
-    }
-    message.channel.send(":white_check_mark: Spam mp de piaf en cour...")
-}
-module.exports.help = {
-    name: "piou"
-  }
---
+++ commands/repeat.js
@@ -0,0 +1,19 @@
+module.exports.run = async (client, message, args) => {
+    if(!args[0]){
+        message.channel.send("choisi un nombre de répétitions");
+        return;
+    }
+    if(args = 0 || args > 100){
+        message.channel.send("choisi un nombre entre 1 et 100")   
+    }
+    if(!args[1]){
+    message.channel.send("Spécifier le texte à répéter.")   
+    }
+    for(var i = 0; i < args[0]; i++){
+        message.channel.send(message.content.substr(9 + args[].length))
+    }
+    message.channel.send(":white_check_mark: répétitions en cour...")
+}
+module.exports.help = {
+    name: "repeat"
+  }
