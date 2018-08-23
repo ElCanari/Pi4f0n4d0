32556baddbbb77e09513e66788c3d54d7b6add0d
pafad <32877302+pafad@users.noreply.github.com>
2018-08-23 03:36:18 +0000

    Update roulette.js

--- commands/roulette.js
+++ commands/roulette.js
@@ -1,6 +1,9 @@
 module.exports.run = async (client, message, args) => {
-    if(args.lenght = 0)return message.channel.send(":x: Tu n'as rien écrit");
+    if(args.lenght = 0){
+        message.channel.send(":x: Tu n'as rien écrit");
+        }else{
     message.channel.send(`🏆 Le vainqueur de **${message.content.substr(11)}** est ${message.guild.members.random().user.tag}`)
+}
 }
 
 module.exports.help = {
