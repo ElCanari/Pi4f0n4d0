f2f083dd7e7eba51a978355092dfa6ebad51d949
pafad <32877302+pafad@users.noreply.github.com>
2018-09-03 10:21:15 +0000

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args) => {
     if(message.guild.id === process.env.rgk){
-      if(!message.member.hasPermission("MANAGE_MESSAGES")){
+      if(!message.member.hasPermission("ADMINISTRATOR")){
         message.channel.send(":x: Tu n'as pas la permission.")
         return;
       }else{
@@ -8,19 +8,17 @@ module.exports.run = async (client, message, args) => {
       message.guild.members.filter(m => m.roles.find("id", args[0])).map(u => u.send(message.content.substr(29)))
       message.channel.send(":baby_chick: annonce envoyée !")
         }
-    }else{
+        
+        
     if(message.guild.id === "388391633298784276"){
-      if(!message.member.hasPermission("MANAGE_MESSAGES")){
+      if(!message.member.hasPermission("ADMINISTRATOR")){
         message.channel.send(":x: Tu n'as pas la permission.")
         return;
       }else{
       if(!args[0]) return message.channel.send(":x: écris quelque chose d'important à annoncer");
       message.guild.members.filter(m => m.roles.find("id", args[0])).map(u => u.send(message.content.substr(29)))
       message.channel.send(":baby_chick: annonce envoyée !")
-        }
-    }else{
-      message.channel.send("Seul Ss army et rgk ont accès à la commande")
-        return;
+      }
 }
   
   module.exports.help = {
