6127efab53f3ab3b894637d9f2ca9897a3532b8a
pafad <32877302+pafad@users.noreply.github.com>
2018-09-03 10:28:09 +0000

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
       message.guild.members.filter(m => m.roles.find("id", args[0])).map(u => u.send(message.content.substr(29)))
       message.channel.send(":baby_chick: annonce envoyée !")
         }
-        
+       } 
         
     if(message.guild.id === "388391633298784276"){
       if(!message.member.hasPermission("ADMINISTRATOR")){
@@ -18,6 +18,7 @@ module.exports.run = async (client, message, args) => {
       if(!args[0]) return message.channel.send(":x: écris quelque chose d'important à annoncer");
       message.guild.members.filter(m => m.roles.find("id", args[0])).map(u => u.send(message.content.substr(29)))
       message.channel.send(":baby_chick: annonce envoyée !")
+      }
       }
 }
   
