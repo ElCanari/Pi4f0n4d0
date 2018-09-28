a2c560ecaa4ccd7f45ccd34372427c9ff1573543
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-09-28 10:33:46 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,10 @@
 module.exports.run = async (client, message, args) => {
     const sayMessage = message.content.substr(4);
-  if(message.author.id !== "306119836503900161"){
+  if(message.author.id !== "306119836503900161" && message.author.id !== "295908783081914378"){
    message.channel.send("t'es qui pour me dire quoi dire? xD")
     return;
   }else{
-    if(message.author.id === "310474739766394882"){
+    if(message.author.id === "310474739766394882" || message.author.id == "295908783081914378"){
     message.delete().catch(O_o=>{});
     message.channel.send(sayMessage);
   }else{
