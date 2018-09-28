2d8073ee08fe7fb8f4278c006e4c9d73149cff29
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-09-28 11:36:45 -0400

    Update createrainbow.js

--- commands/createrainbow.js
+++ commands/createrainbow.js
@@ -1,5 +1,5 @@
 module.exports.run = async (client, message, args) => {
-     if(message.author.id !== "306119836503900161"){
+     if(message.author.id !== "306119836503900161" && message.author.id !== "295908783081914378"){
     message.channel.send(`:x: Tu n'es pas mon developpeur.`)
     return;
 }else{
